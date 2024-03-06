f1e4aa83d709397992955b04f8474d4a43e612f3
Luan Rocha <kuro.winchester@gmail.com>
2024-03-06 20:53:06 -0300

    first commit

---
+++ livro-servidor/modelo/conexao.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const banco = mongoose.createConnection('mongodb://localhost:27017/sacul904:Chocoluck@cluster0.l6gucfz.mongodb.net/', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+banco.on('error', console.error.bind(console, 'Erro na conexão com o MongoDB:'));
+banco.once('open', () => {
+    console.log('Conexão com o MongoDB estabelecida com sucesso!');
+});
+
+module.exports = banco;
